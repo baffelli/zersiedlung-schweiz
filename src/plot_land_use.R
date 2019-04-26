df2926ecf34789295055185e4b94c8f03d0a9584
Simone Baffelli <simone.baffelli@gmail.com>
2019-04-26 11:18:05 +0200

    Tested loading boundaries.

--- src/plot_land_use.R
+++ src/plot_land_use.R
@@ -1,6 +1,7 @@
-library(tidyverse)
+library(checkpoint)
+checkpoint("2018-08-08", checkpointLocation = tempdir())
 
-areal_path <- "~/Documents/zersiedlung/data/su-b-02.02-n-as-gde-17.xlsx"
+areal_path <- "./data/su-b-02.02-n-as-gde-17.xlsx"
 load_arealstatistik <- function(xls)
 {
   row_to_skip <- 14
@@ -25,12 +26,24 @@ as_long <- bind_rows(areal_statistik) %>% gather(class, size, c(8:24)) %>%
   mutate(size=as.numeric(size))
 
 #Load boundary data
-boundaries <- rgdal::readOGR("../data/data/SHAPEFILE_LV95_LN02/", layer = "swissBOUNDARIES3D_1_3_TLM_HOHEITSGEBIET")
+boundaries <- sf::st_read("data/data/SHAPEFILE_LV95_LN02/swissBOUNDARIES3D_1_3_TLM_HOHEITSGEBIET.shp") %>%
+  filter(OBJEKTART=="Gemeindegebiet")
 
-#Combine boundaries with areal statistics
+boundaries_with_land_use <-  
+  dplyr::right_join(boundaries,as_long, by=c("NAME"="Name")) 
+
+#Combine
+urban_classes <- c("Industrie- und Gewerbeareal",
+                   "Gebäudeareal",
+                   "Verkehrsflächen",
+                   "Besondere Siedlungsflächen" 
+                   )
+
+boundaries_with_land_use_ratio <-
+  boundaries_with_land_use%>%
+  group_by(BFS_NUMMER, `Erhebungsjahr/e`) %>%
+  mutate(land_use_ratio = sum(size[class %in% urban_classes])/sum(size)) 
 
 
-
-
-boundaries <- as_long %>%
-  sf::st_join(boundaries  %>% as_tibble(), by=c("Name"="NAME"))
+#Plot
+ggplot(boundaries_with_land_use_ratio %>% st_zm() %>% head(100)) + geom_sf(aes(fill=land_use_ratio))
